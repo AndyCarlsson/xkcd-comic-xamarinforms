678c78623231e5ccf3455218bf66cc31924f6404
AndyCarlsson <andreas95carlsson@gmail.com>
2020-06-10 16:44:51 +0200

    Created App. First commit

---
+++ xkcd_comics/xkcd_comics/xkcd_comics/Models/Comic.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace xkcd_comics.Models
+{
+    public class Comic
+    {
+        public string Month { get; set; }
+        public int Num { get; set; }
+        public string Link { get; set; }
+        public string Year { get; set; }
+        public string News { get; set; }
+        public string Safe_title { get; set; }
+        public string Transcript { get; set; }
+        public string Alt { get; set; }
+        public string Img { get; set; }
+        public string Title { get; set; }
+        public string Day { get; set; }
+    }
+}
---
+++ xkcd_comics/xkcd_comics/xkcd_comics/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using xkcd_comics.Models;
+
+namespace xkcd_comics.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
