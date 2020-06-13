b50bc3d6ab65aea22b865e749df23b87128f5d2a
AndyCarlsson <andreas95carlsson@gmail.com>
2020-06-13 16:33:18 +0200

    Added buttons with commands

--- xkcd_comics/xkcd_comics/xkcd_comics/ViewModels/ComicPageViewModel.cs
+++ xkcd_comics/xkcd_comics/xkcd_comics/ViewModels/ComicPageViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
 using xkcd_comics.Models;
 
 namespace xkcd_comics.ViewModels
@@ -8,13 +10,19 @@ namespace xkcd_comics.ViewModels
     class ComicPageViewModel : BaseViewModel
     {
         public Comic Comic { get; set; }
+        public ICommand GetRandomComicCommand { get; set; }
         public ComicPageViewModel()
         {
             Title = "Comic";
             Comic = new Comic();
+            Comic = DataService.GetComicAsync("678");
 
-            Comic = DataService.GetComicAsync("341");
+            GetRandomComicCommand = new Command(GetRandomComic);
         }
 
+        private void GetRandomComic()
+        {
+            Comic = DataService.GetComicAsync("100");
+        }
     }
 }
